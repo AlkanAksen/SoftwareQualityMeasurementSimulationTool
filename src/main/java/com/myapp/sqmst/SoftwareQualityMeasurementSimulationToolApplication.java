eafef433e2d26338868a08d8636a1a6d4de6839c
AksenAlkan <alkanaksen1@gmail.com>
2025-11-01 21:11:46 +0300

    Initial commit

---
+++ src/main/java/com/myapp/sqmst/SoftwareQualityMeasurementSimulationToolApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.sqmst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareQualityMeasurementSimulationToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareQualityMeasurementSimulationToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myapp/sqmst/SoftwareQualityMeasurementSimulationToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.sqmst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareQualityMeasurementSimulationToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
