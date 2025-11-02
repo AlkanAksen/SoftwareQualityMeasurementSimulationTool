62f88d85d76ab26fac91018f640a7f37c64a5191
Ahmet Buğra Kara <ahmetbugrakara297326@gmail.com>
2025-11-02 15:19:34 +0300

    Update SoftwareQualityMeasurementSimulationToolApplication.java

--- src/main/java/com/myapp/sqmst/SoftwareQualityMeasurementSimulationToolApplication.java
+++ src/main/java/com/myapp/sqmst/SoftwareQualityMeasurementSimulationToolApplication.java
@@ -3,11 +3,17 @@ package com.myapp.sqmst;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Map;
+
 @SpringBootApplication
 public class SoftwareQualityMeasurementSimulationToolApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SoftwareQualityMeasurementSimulationToolApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(SoftwareQualityMeasurementSimulationToolApplication.class);
+        app.setDefaultProperties(Map.of(
+                "server.address", "0.0.0.0",
+                "server.port", "8080"
+        ));
+        app.run(args);
+    }
 }
